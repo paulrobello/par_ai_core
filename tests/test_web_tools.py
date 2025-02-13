2ccdef18baa53cf8d802b95f01b1c146b977808f
Paul Robello <probello@gmail.com>
2025-02-12 18:22:57 -0800

    style: Reorder imports in test_web_tools.py

--- tests/test_web_tools.py
+++ tests/test_web_tools.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from unittest.mock import MagicMock, patch, call
+from unittest.mock import MagicMock, call, patch
 
 import pytest
 from bs4 import BeautifulSoup
